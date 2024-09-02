0b3ca609170c0df6c8b87d91e26587f10542c910
Soumya Snigdha Kundu <soumya_snigdha.kundu@kcl.ac.uk>
2024-09-02 12:25:06 +0100

    optimised code for connected components with gradients

--- losses/tools.py
+++ losses/tools.py
@@ -69,34 +69,34 @@ def connected_components_with_gradients(
         ) -> torch.Tensor:
 
     if spatial_dims == 3:
-        H, W, Z = image.shape[-3:]
+        B, C, H, W, Z = image.shape
 
-        ## precompute a mask with the valid values
-        mask = torch.zeros((1, 1, H, W, Z), dtype=torch.bool, device=image.device)
-        mask[image >= threshold] = True
+        # Precompute a mask with the valid values
+        mask = image >= threshold
         image = torch.ceil(image * mask)
-        
-        B = image.shape[0]
-        out = torch.arange(B * H * W * Z, device=image.device, dtype=image.dtype).view(-1, 1, H, W, Z)
-        out[~mask] = 0.
 
+        out = torch.arange(B * H * W * Z, device=image.device, dtype=image.dtype).view(B, 1, H, W, Z)
+        out = torch.where(mask, out, torch.tensor(0, device=image.device, dtype=image.dtype))
+
+        # Use in-place operations to potentially speed up the loop
         for _ in range(num_iterations):
-            out[mask] = F.max_pool3d(out, kernel_size=3, stride=1, padding=1)[mask]
+            out = F.max_pool3d(out, kernel_size=3, stride=1, padding=1)
+            out = torch.where(mask, out, torch.tensor(0, device=image.device, dtype=image.dtype))
 
     elif spatial_dims == 2:
-        H, W = image.shape[-2:]
+        B, C, H, W = image.shape
 
-        ## precompute a mask with the valid values
-        mask = torch.zeros((1, 1, H, W), dtype=torch.bool, device=image.device)
-        mask[image >= threshold] = True
+        # Precompute a mask with the valid values
+        mask = image >= threshold
         image = torch.ceil(image * mask)
-        
-        B = image.shape[0]
-        out = torch.arange( B * H * W, device=image.device, dtype=image.dtype).view((-1, 1, H, W))
-        out[~mask] = 0.
 
+        out = torch.arange(B * H * W, device=image.device, dtype=image.dtype).view(B, 1, H, W)
+        out = torch.where(mask, out, torch.tensor(0, device=image.device, dtype=image.dtype))
+
+        # Use in-place operations to potentially speed up the loop
         for _ in range(num_iterations):
-            out[mask] = F.max_pool2d(out, kernel_size=3, stride=1, padding=1)[mask]
+            out = F.max_pool2d(out, kernel_size=3, stride=1, padding=1)
+            out = torch.where(mask, out, torch.tensor(0, device=image.device, dtype=image.dtype))
 
     return image * out
 
